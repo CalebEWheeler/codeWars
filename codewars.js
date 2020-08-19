6365f909bd6a6e820973a25d839b5f04cabe55c8
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-08-19 12:50:37 -0500

    completed minutes to seconds function

---
+++ codewars.js
@@ -0,0 +1,5 @@
+// <==========================          first problem          ==========================>
+function spinWords(){
+    //TODO Have fun :)
+    return
+}
---
+++ edabit.js
@@ -0,0 +1,10 @@
+// <==========================          first problem          ==========================>
+
+//Write a function that takes an integer minutes and
+//converts it to seconds.
+function convert(minutes) {
+    return minutes*60;
+}
+
+// <==========================          second problem          ==========================>
+

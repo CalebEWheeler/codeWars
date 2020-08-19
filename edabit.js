505e1f0b96ab2c33ba483d3190ab3d084b5ccab6
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-08-19 13:43:28 -0500

    completed squared function, had to fix all of the errors present for the functions to pass.

--- edabit.js
+++ edabit.js
@@ -45,3 +45,12 @@ function addition(num) {
 
 // <==========================          fifth problem          ==========================>
 
+//Fix the code in the code tab to pass this challenge
+//(only syntax errors). Look at the examples below to
+//get an idea of what the function should do.
+function squared(b) {
+    return b * b
+}
+
+// <==========================          sixth problem          ==========================>
+

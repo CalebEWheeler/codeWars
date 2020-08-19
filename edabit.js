e9e2381fbcf0636620223f7a174f619124b213d3
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-08-19 12:52:40 -0500

    completed findPerimeter function

--- edabit.js
+++ edabit.js
@@ -8,3 +8,10 @@ function convert(minutes) {
 
 // <==========================          second problem          ==========================>
 
+//Create a function that takes length and width and
+//finds the perimeter of a rectangle.
+function findPerimeter(length, width) {
+    return (length *2) + (width *2)
+}
+
+// <==========================          third problem          ==========================>

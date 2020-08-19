1c71cd162efa64c068f0f34f2bab484434513237
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-08-19 12:57:33 -0500

    completed isSameNum function

--- edabit.js
+++ edabit.js
@@ -22,5 +22,17 @@ function checkEquality(a, b) {
     return a === b
 }
 
-// <==========================          third problem          ==========================>
+// <==========================          fourth problem          ==========================>
+
+//Create a function that returns true when num1 is
+//equal to num2; otherwise return false.
+function isSameNum(num1, num2) {
+    if (num1 !== num2) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// <==========================          fifth problem          ==========================>
 

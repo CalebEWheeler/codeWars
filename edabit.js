a3b3ebe18135eac19ec62a988ce2deeec8f171be
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-08-19 21:01:43 -0500

    started findBob()

--- edabit.js
+++ edabit.js
@@ -100,6 +100,9 @@ function nextEdge(side1, side2) {
 }
 
 // <==========================          eleventh problem          ==========================>
+//Write a function that searches an array of names (unsorted) for the name
+//"Bob" and returns the location in the array. If Bob is not in the array,
+//return -1.
 
 
 // <==========================         twelfth problem          ==========================>

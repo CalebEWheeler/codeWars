5ad22d6a05debdaabedaabffef2a220a01a174a2
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-08-19 20:55:39 -0500

    completed nextEdge()

--- edabit.js
+++ edabit.js
@@ -90,7 +90,14 @@ function dividesEvenly(a, b) {
 }
 
 // <==========================          tenth problem          ==========================>
-
+//Create a function that finds the maximum range of a triangle's third edge, '
+//+ 'where the side lengths are all integers.
+//(side1 + side2) - 1 = maximum range of third edge.
+//The side lengths of the triangle are positive integers.
+//Don't forget to return the result.
+function nextEdge(side1, side2) {
+    return ((side1 + side2)-1)
+}
 
 // <==========================          eleventh problem          ==========================>
 
